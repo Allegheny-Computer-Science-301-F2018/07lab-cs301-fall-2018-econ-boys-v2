7ed559b4c6da60b680e4be8bd3a04c28bc879015
jerfenson <cerdamejiaj@allegheny.edu>
2018-11-26 21:32:56 -0500

    uploadong veriables that we will use for analysis

--- src/analysis.r
+++ src/analysis.r
@@ -9,18 +9,24 @@
 
 
 
-###### Useful libraries:
+###### Useful libraries:vir
 library(dplyr)
 library(tidyverse)
 library(psych)
+library(readxl)
 
 
 
 ##### Begin by loading your csv file from the supplimental/ directory.
 
 data <- read.table(file.choose(),fill = TRUE , header = TRUE, sep = ",")
-Australia <- filter(data, Country.Name == "Australia")
-Argentina <- filter(data, Country.Name = "Argentina")
+
+my_data <- read_excel(file.choose())
+Australia <- filter(my_data, Country == "Australia")
+Canada <- filter(my_data, Country == "Canada")
+
+names(my_data)[1] <- "Country"
+names(my_data)[3] <- "IndicatorName"
 
 
 
@@ -31,17 +37,68 @@ Argentina <- filter(data, Country.Name = "Argentina")
 ##### Question 1: The decline in marriage rates.
 ##### Analysis to respond to this research question.
 
+CanadaAnalysis <- filter(Canada[c(25, 75, 77),])
+AustraliaAnalysis <- filter(Australia[c(25, 75, 77),])
+                          
+                           #Canada 
+# Time required to start a business, female (days
+# Average number of hours spent on unpaid domestic work (housework and child care), female
+# Contributing family workers, female (% of female employment)   
+
+
+                          #Australia
+# Time required to start a business, female (days
+# Average number of hours spent on unpaid domestic work (housework and child care), female
+# Contributing family workers, female (% of female employment)   
+
+
+
 
 
 ##### Question 2: The narrowing gender wage gap
 ##### Analysis to respond to this research question.
 
+                         #Canada 
+# Wage and salaried workers, female (% of female employment)
+# Educational attainment, completed Bachelor's or equivalent, population 25+ years, female (%)
+# Employment in industry, female (% of female employment)
+
+
+
+
+                        #Australia
+# Wage and salaried workers, female (% of female employment)
+# Educational attainment, completed Bachelor's or equivalent, population 25+ years, female (%)
+# Employment in industry, female (% of female employment)
 
 
 ##### Question 3: The preference (or cultural) shift towards market work
 ##### Analysis to respond to this research question.
 
 
+                        #Canada
+# Labor force participation rate, female (% of female population ages 15+) (national estimate)
+# Employers, female (% of female employment)
+# Labor force, female (% of total labor force)
 
-##### Question 4: The change in women’s bargaining power within the household.
+
+                      #Australia
+
+# Labor force participation rate, female (% of female population ages 15+) (national estimate)
+# Employers, female (% of female employment)
+# Labor force, female (% of total labor force)
+
+
+##### Question 4: The change in woman bargaining power within the household.
 ##### Analysis to respond to this research question.
+
+
+                      #Canada
+# Contributing family workers, female (% of female employment) 
+#Wage and salaried workers, female (% of female employment) comparing to males 
+# elf-employed, female (% of female employment)
+
+                    #Australia
+# Contributing family workers, female (% of female employment)  
+# Wage and salaried workers, female (% of female employment) comparing to males 
+# elf-employed, female (% of female employment)
