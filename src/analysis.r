becc3b92080400c0387d481532473739c3a31b1c
Zachary Leonardo <leonardoz@aldenv160.allegheny.edu>
2018-11-28 11:03:07 -0500

    finished analysis

--- src/analysis.r
+++ src/analysis.r
@@ -90,20 +90,15 @@ summary(Canada_Mod1)
                            #Canada 
 # Time required to start a business, female (days
 # Contributing family workers, female (% of female employment)   
-
-#mod <- lm(LungCap ~ Age + Height)
-#summary(mod)
-
-
                           #Australia
 # Time required to start a business, female (days
 # Contributing family workers, female (% of female employment)   
 
 
-
 ##### Question 2: The narrowing gender wage gap
 ##### Analysis to respond to this research question.
 
+#Start of Australia analysis:
 CanadaAnalysisQ2 <- filter(Canada[c(121, 148, 498),])
 AustraliaAnalysisQ2 <- filter(Australia[c(121, 148, 498),])
 
@@ -115,15 +110,12 @@ AustraliaAnalysisQ2<- cbind(rownames(AustraliaAnalysisQ2), AustraliaAnalysisQ2)
 rownames(AustraliaAnalysisQ2) <- NULL
 names(AustraliaAnalysisQ2)[1] <- "Year"
 attach(AustraliaAnalysisQ2)
-
-
 #plot for Education:
 ggplot(AustraliaAnalysisQ2) + geom_point(mapping = aes(x = Year, y = Educational_BachelorA))
 #plot for Employment:
 ggplot(AustraliaAnalysisQ2) + geom_point(mapping = aes(x = Year, y = Employment_industryA))
 #plot for Wages and Salried:
 ggplot(AustraliaAnalysisQ2) + geom_point(mapping = aes(x = Year, y = Wage_salariedA))
-
 #To generate p-value:
 Australia_Education <- unfactor(Educational_BachelorA)
 Australia_Employment <- unfactor(Employment_industryA)
@@ -132,9 +124,7 @@ AustraliaQ2_Mod1 <- lm(Australia_Employment ~ Australia_Education + Australia_Wa
 summary(AustraliaQ2_Mod1)
 #End of Australia Analysis
 
-
-
-
+#Start of Canada analysis:
 CanadaAnalysisQ2 <- as.data.frame(t(CanadaAnalysisQ2))
 CanadaAnalysisQ2 <- CanadaAnalysisQ2[-c(1,2,4),]
 colnames(CanadaAnalysisQ2) <- c("Educational_BachelorC", "Employment_industryC", "Wage_salariedC")
@@ -143,30 +133,24 @@ CanadaAnalysisQ2 <- cbind(rownames(CanadaAnalysisQ2), CanadaAnalysisQ2)
 rownames(CanadaAnalysisQ2) <- NULL
 names(CanadaAnalysisQ2)[1] <- "Year"
 attach(CanadaAnalysisQ2)
-
 #plot for Education:
 ggplot(CanadaAnalysisQ2) + geom_point(mapping = aes(x = Year, y = Educational_BachelorC))
 #plot for Employment:
 ggplot(CanadaAnalysisQ2) + geom_point(mapping = aes(x = Year, y = Employment_industryC))
 #plot for Wages and Salried:
 ggplot(CanadaAnalysisQ2) + geom_point(mapping = aes(x = Year, y = Wage_salariedC))
-
 #To generate p-value:
 Canada_Education <- unfactor(Educational_BachelorC)
 Canada_Employment <- unfactor(Employment_industryC)
 Canada_Wages_Salaries <- unfactor(Wage_salariedC)
 CanadaQ1_Mod1 <- lm(Canada_Employment ~ Canada_Education + Canada_Wages_Salaries)
 summary(CanadaQ1_Mod1)
-#End of Australia Analysis
+#End of Canada analysis
 
                          #Canada 
 # Wage and salaried workers, female (% of female employment)
 # Educational attainment, completed Bachelor's or equivalent, population 25+ years, female (%)
 # Employment in industry, female (% of female employment)
-
-
-
-
                         #Australia
 # Wage and salaried workers, female (% of female employment)
 # Educational attainment, completed Bachelor's or equivalent, population 25+ years, female (%)
@@ -176,15 +160,62 @@ summary(CanadaQ1_Mod1)
 ##### Question 3: The preference (or cultural) shift towards market work
 ##### Analysis to respond to this research question.
 
+#Start of Australia analysis:
+CanadaAnalysisQ3 <- filter(Canada[c(208, 144, 218),])
+AustraliaAnalysisQ3 <- filter(Australia[c(208, 144, 218),])
+
+AustraliaAnalysisQ3 <- as.data.frame(t(AustraliaAnalysisQ3))
+AustraliaAnalysisQ3 <- AustraliaAnalysisQ3[-c(1,2,4),]
+colnames(AustraliaAnalysisQ3) <- c("participationA", "employersA", "total_laborA")
+AustraliaAnalysisQ3 <- AustraliaAnalysisQ3[-c(1),]
+AustraliaAnalysisQ3 <- AustraliaAnalysisQ3[-c(1:19), ]
+AustraliaAnalysisQ3 <- cbind(rownames(AustraliaAnalysisQ3), AustraliaAnalysisQ3)
+rownames(AustraliaAnalysisQ3) <- NULL
+names(AustraliaAnalysisQ3)[1] <- "Year"
+attach(AustraliaAnalysisQ3)
+#plot for female labor force participation rate:
+ggplot(AustraliaAnalysisQ3) + geom_point(mapping = aes(x = Year, y = participationA))
+#plot for female employers as % of femal employment:
+ggplot(AustraliaAnalysisQ3) + geom_point(mapping = aes(x = Year, y = employersA))
+#plot for total female labor as % of labor force:
+ggplot(AustraliaAnalysisQ3) + geom_point(mapping = aes(x = Year, y = total_laborA))
+#To generate p-value:
+Australia_participation <- unfactor(participationA)
+Australia_employers <- unfactor(employersA)
+Australia_Labor <- unfactor(total_laborA)
+Australia_Mod3 <- lm(Australia_Labor ~ Australia_participation + Australia_employers)
+summary(Australia_Mod3)
+#End of Australia analysis
+
+#Start of Canada analysis:
+CanadaAnalysisQ3 <- as.data.frame(t(CanadaAnalysisQ3))
+CanadaAnalysisQ3 <- CanadaAnalysisQ3[-c(1,2,4),]
+colnames(CanadaAnalysisQ3) <- c("participationC", "employersC", "total_laborC")
+CanadaAnalysisQ3 <- CanadaAnalysisQ3[-c(1),]
+CanadaAnalysisQ3 <- CanadaAnalysisQ3[-c(1:16), ]
+CanadaAnalysisQ3 <- cbind(rownames(CanadaAnalysisQ3), CanadaAnalysisQ3)
+rownames(CanadaAnalysisQ3) <- NULL
+names(CanadaAnalysisQ3)[1] <- "Year"
+attach(CanadaAnalysisQ3)
+#plot for female labor force participation rate:
+ggplot(CanadaAnalysisQ3) + geom_point(mapping = aes(x = Year, y = participationA))
+#plot for female employers as % of femal employment:
+ggplot(CanadaAnalysisQ3) + geom_point(mapping = aes(x = Year, y = employersA))
+#plot for total female labor as % of labor force:
+ggplot(CanadaAnalysisQ3) + geom_point(mapping = aes(x = Year, y = total_laborA))
+#To generate p-value:
+Canada_participation <- unfactor(participationA)
+Canada_employers <- unfactor(employersA)
+Canada_Labor <- unfactor(total_laborA)
+Canada_Mod3 <- lm(Canada_Labor ~ Canada_participation + Canada_employers)
+summary(Canada_Mod3)
+#End of Cananda analysis
 
                         #Canada
 # Labor force participation rate, female (% of female population ages 15+) (national estimate)
 # Employers, female (% of female employment)
 # Labor force, female (% of total labor force)
-
-
                       #Australia
-
 # Labor force participation rate, female (% of female population ages 15+) (national estimate)
 # Employers, female (% of female employment)
 # Labor force, female (% of total labor force)
@@ -193,10 +224,59 @@ summary(CanadaQ1_Mod1)
 ##### Question 4: The change in woman bargaining power within the household.
 ##### Analysis to respond to this research question.
 
+#Start of Australia analysis:
+CanadaAnalysisQ4 <- filter(Canada[c(75, 498, 436),])
+AustraliaAnalysisQ4 <- filter(Australia[c(75, 498, 436),])
 
+AustraliaAnalysisQ4 <- as.data.frame(t(AustraliaAnalysisQ4))
+AustraliaAnalysisQ4 <- AustraliaAnalysisQ4[-c(1,2,4),]
+colnames(AustraliaAnalysisQ4) <- c("contributingA", "salariedA", "self_empA")
+AustraliaAnalysisQ4 <- AustraliaAnalysisQ4[-c(1),]
+AustraliaAnalysisQ4 <- AustraliaAnalysisQ4[-c(1:30), ]
+AustraliaAnalysisQ4 <- cbind(rownames(AustraliaAnalysisQ4), AustraliaAnalysisQ4)
+rownames(AustraliaAnalysisQ4) <- NULL
+names(AustraliaAnalysisQ4)[1] <- "Year"
+attach(AustraliaAnalysisQ4)
+#plot for contribution:
+ggplot(AustraliaAnalysisQ4) + geom_point(mapping = aes(x = Year, y = contributingA))
+#plot for salaried female workers as % of femal employmen:
+ggplot(AustraliaAnalysisQ4) + geom_point(mapping = aes(x = Year, y = salariedA))
+#plot for self employeed female workers:
+ggplot(AustraliaAnalysisQ4) + geom_point(mapping = aes(x = Year, y = self_empA))
+#To generate p-value:
+Australia_contributionQ4 <- unfactor(contributingA)
+Australia_salaried <- unfactor(salariedA)
+Australia_self <- unfactor(self_empA)
+Australia_Mod4 <- lm(Australia_salaried ~ Australia_contributionQ4 + Australia_self)
+summary(Australia_Mod4)
+#End of Australia analysis
+
+#Start of Canada analysis:
+CanadaAnalysisQ4 <- as.data.frame(t(CanadaAnalysisQ4))
+CanadaAnalysisQ4 <- CanadaAnalysisQ4[-c(1,2,4),]
+colnames(CanadaAnalysisQ4) <- c("contributingC", "salariedC", "self_empC")
+CanadaAnalysisQ4 <- CanadaAnalysisQ4[-c(1),]
+CanadaAnalysisQ4 <- CanadaAnalysisQ4[-c(1:16), ]
+CanadaAnalysisQ4 <- cbind(rownames(CanadaAnalysisQ4), CanadaAnalysisQ4)
+rownames(CanadaAnalysisQ4) <- NULL
+names(CanadaAnalysisQ4)[1] <- "Year"
+attach(CanadaAnalysisQ4)
+#plot for contribution:
+ggplot(CanadaAnalysisQ4) + geom_point(mapping = aes(x = Year, y = contributingC))
+#plot for salaried female workers as % of femal employmen:
+ggplot(CanadaAnalysisQ4) + geom_point(mapping = aes(x = Year, y = salariedC))
+#plot for self employed female workers:
+ggplot(CanadaAnalysisQ4) + geom_point(mapping = aes(x = Year, y = self_empC))
+#To generate p-value:
+Canada_contributionQ4 <- unfactor(contributingC)
+Canada_salaried <- unfactor(salariedC)
+Canada_self <- unfactor(self_empC)
+Canada_Mod4 <- lm(Canada_salaried ~ Canada_contributionQ4+ Canada_self)
+summary(Canada_Mod4)
+#End of Canada analysis
                       #Canada
 # Contributing family workers, female (% of female employment) 
-#Wage and salaried workers, female (% of female employment) comparing to males 
+# Wage and salaried workers, female (% of female employment) comparing to males 
 # elf-employed, female (% of female employment)
 
                     #Australia
